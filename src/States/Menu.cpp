b179dfd4dfece7f9143fdba2360e0a221e947270
LuigiSouza <lpsouza@inf.ufsm.br>
2021-07-20 14:08:09 -0300

    feat: menu working

--- src/States/Menu.cpp
+++ src/States/Menu.cpp
@@ -7,6 +7,26 @@ void Menu::render(void)
 
 void Menu::update(Mouse mouse)
 {
+    if (mouse.pushed(0))
+    {
+        Botao *button = panel->buttonClicked(mouse);
+        if (!button)
+            return;
+        switch (button->get_function())
+        {
+        case EnumBotao::Jogar:
+            this->change_state = true;
+            break;
+        case EnumBotao::Facil:
+            button->set_function(EnumBotao::Dificil);
+            this->difficult = EnumBotao::Dificil;
+            break;
+        case EnumBotao::Dificil:
+            button->set_function(EnumBotao::Facil);
+            this->difficult = EnumBotao::Facil;
+            break;
+        }
+    }
 }
 
 void Menu::dispose(void)
@@ -15,7 +35,13 @@ void Menu::dispose(void)
 
 Menu::Menu(float x, float y, float width, float height)
 {
+    change_state = false;
+
     panel = new Panel(x, y, width, height);
+    panel->addButton(width / 3, height - 100, 80, 50, EnumBotao::Jogar, 3);
+    panel->addButton(width / 3, height - 200, 80, 50, EnumBotao::Facil, 5);
+
+    difficult = EnumBotao::Facil;
 }
 
 Menu::~Menu()
